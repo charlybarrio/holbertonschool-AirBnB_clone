c350dd821a1365f533d3ae8bace81092b8199f16
C-Mauro <maurocamila744@gmail.com>
2023-02-25 08:37:53 -0800

    call save() method of storage

--- models/base_model.py
+++ models/base_model.py
@@ -17,12 +17,13 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-
+            models.storage.new(self)
     def __str__(self):
         return "[{}] ({}) ({})".format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
         updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         format_date = '%Y-%m-%dT%H:%M:%S.%f'
