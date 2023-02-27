9d2b5ce410f0a2ce5d181f6bdbe00c8c839ec399
C-Mauro <maurocamila744@gmail.com>
2023-02-27 11:56:05 -0800

    clean code

--- models/user.py
+++ models/user.py
@@ -5,7 +5,6 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     '''Define a user'''
-    def __init__(self):
         self.email = ""
         self.password = ""
         self.first_name = ""
