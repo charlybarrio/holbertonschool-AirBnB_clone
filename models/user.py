a87e9528898cc16147cd381754c93366a190d8bd
C-Mauro <maurocamila744@gmail.com>
2023-02-28 09:34:23 -0800

    fix self in user.py and first commit for test_base_model

--- models/user.py
+++ models/user.py
@@ -5,7 +5,7 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     '''Define a user'''
-    self.email = ""
-    self.password = ""
-    self.first_name = ""
-    self.last_name = ""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
