2fcbdf857936d834a2c97c017bb60d232616ba46
charlybarrio <5757@holbertonstudents.com>
2023-02-28 11:22:31 -0800

    fix json file

--- console.py
+++ console.py
@@ -3,7 +3,13 @@
 import cmd
 import json
 from models import storage
-
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.review import Review
+from models.city import City
+from models.amenity import Amenity
 
 classlist = ["BaseModel", "State", "City", "Amenity"]
 
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,6 +2,12 @@
 '''Store first objects'''
 import json
 from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
 class FileStorage:
     '''Serializes and deserializes file'''
@@ -23,13 +29,14 @@ class FileStorage:
         my_obj = {}
         for k, v in FileStorage.__objects.items():
             my_obj[k] = v.to_dict()
-        json.dump(my_obj, fp)
+        with open(self.__file_path, 'w', encoding='utf-8') as f:
+            json.dump(my_obj, f)
 
     def reload(self):
         '''to python objs'''
         try:
-            with open(FileStorage.__file_path, 'r', encoding="utf-8") as fp:
-                obj_dict = json.load(fp)
+            with open(FileStorage.__file_path, 'r', encoding="utf-8") as f:
+                obj_dict = json.load(f)
                 for k, v in obj_dict.items():
                     cls_name = v['__class__']
                     obj = eval(cls_name)(**v)
